27a955aeb5ba34c06fcc4a8e78f242437d54537a
Bao Ngoc <ngocvuong95@gmail.com>
2026-09-13 23:25:58 +0700

    exersice-js

---
+++ ex1.js
@@ -0,0 +1,5 @@
+const pi = 3.14;
+if (pi<0) {
+    console.log(Nếu hằng số lớn hơn 0: “Giá trị bạn nhập là số dương”
+                Nếu hằng số nhỏ hơn 0: “Giá trị bạn nhập là số âm”)
+}
---
+++ ex2.js
@@ -0,0 +1,4 @@
+let height = 160;
+if (100<height<200) {
+    console.log('Ideal weight = ', (height - 100) *9/10, 'kg,', 'max weight = ', (height - 100), 'kg,' ,'min weight =', (height - 100) *8/10, 'kg' )
+}
